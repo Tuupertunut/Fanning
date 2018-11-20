146d4505819b49efee64e64815af5664bcbccd88
Tuupertunut <superemppu@live.fi>
2018-11-20 19:36:55 +0200

    Added mock hardware implementation.

---
+++ src/main/java/com/github/tuupertunut/fanning/mockhardware/MockControl.java
@@ -0,0 +1,64 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Tuupertunut.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.tuupertunut.fanning.mockhardware;
+
+import com.github.tuupertunut.fanning.hwinterface.Control;
+import com.github.tuupertunut.fanning.hwinterface.Sensor;
+
+/**
+ *
+ * @author Tuupertunut
+ */
+public class MockControl implements Control {
+
+    @Override
+    public Sensor getSensor() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isSoftwareControlled() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getMinSoftwareControlValue() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getMaxSoftwareControlValue() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setSoftwareControl(double value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setDefaultControl() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
---
+++ src/main/java/com/github/tuupertunut/fanning/mockhardware/MockHardwareItem.java
@@ -0,0 +1,75 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Tuupertunut.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.tuupertunut.fanning.mockhardware;
+
+import com.github.tuupertunut.fanning.hwinterface.Control;
+import com.github.tuupertunut.fanning.hwinterface.HardwareItem;
+import com.github.tuupertunut.fanning.hwinterface.Sensor;
+import java.util.List;
+
+/**
+ *
+ * @author Tuupertunut
+ */
+public class MockHardwareItem implements HardwareItem {
+
+    List<HardwareItem> subHardware;
+    List<Sensor> sensors;
+    List<Control> controls;
+    String name;
+    String id;
+
+    public MockHardwareItem(List<HardwareItem> subHardware, List<Sensor> sensors, List<Control> controls, String name, String id) {
+        this.subHardware = subHardware;
+        this.sensors = sensors;
+        this.controls = controls;
+        this.name = name;
+        this.id = id;
+    }
+
+    @Override
+    public List<HardwareItem> getSubHardware() {
+        return subHardware;
+    }
+
+    @Override
+    public List<Sensor> getSensors() {
+        return sensors;
+    }
+
+    @Override
+    public List<Control> getControls() {
+        return controls;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+}
