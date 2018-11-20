9094d7a843d4bffbdfccd42e7197d08066b065f6
Tuupertunut <superemppu@live.fi>
2018-11-20 22:25:13 +0200

    Added primitive TreeTableView for displaying sensor data in the GUI.

--- src/main/java/com/github/tuupertunut/fanning/FanningPane.java
+++ src/main/java/com/github/tuupertunut/fanning/FanningPane.java
@@ -23,9 +23,17 @@
  */
 package com.github.tuupertunut.fanning;
 
+import com.github.tuupertunut.fanning.hwinterface.HardwareItem;
+import com.github.tuupertunut.fanning.hwinterface.HardwareManager;
+import com.github.tuupertunut.fanning.hwinterface.Sensor;
+import com.github.tuupertunut.fanning.mockhardware.MockHardwareManager;
 import java.io.IOException;
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -33,6 +41,9 @@ import javafx.scene.layout.AnchorPane;
  * @author Tuupertunut
  */
 public class FanningPane extends AnchorPane {
+
+    @FXML
+    private TreeTableView<SensorOrHardwareItemWrapper> sensorTreeTable;
 
     public FanningPane() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FanningPane.fxml"));
@@ -48,5 +59,38 @@ public class FanningPane extends AnchorPane {
 
     @FXML
     private void initialize() {
+        TreeTableColumn<SensorOrHardwareItemWrapper, String> sensorNameColumn = new TreeTableColumn<>("Sensor");
+        sensorNameColumn.setPrefWidth(300);
+        sensorNameColumn.setCellValueFactory((TreeTableColumn.CellDataFeatures<SensorOrHardwareItemWrapper, String> data) -> {
+
+            return new ReadOnlyStringWrapper(data.getValue().getValue().getName());
+        });
+
+        TreeTableColumn<SensorOrHardwareItemWrapper, String> sensorValueColumn = new TreeTableColumn<>("Value");
+        sensorValueColumn.setCellValueFactory((TreeTableColumn.CellDataFeatures<SensorOrHardwareItemWrapper, String> data) -> {
+
+            return new ReadOnlyStringWrapper(data.getValue().getValue().getValue());
+        });
+
+        sensorTreeTable.getColumns().setAll(sensorNameColumn, sensorValueColumn);
+
+
+        HardwareManager hwManager = new MockHardwareManager();
+        TreeItem<SensorOrHardwareItemWrapper> root = createTreeTableModel(hwManager.getHardwareRoot().get());
+
+        sensorTreeTable.setRoot(root);
+    }
+
+    private TreeItem<SensorOrHardwareItemWrapper> createTreeTableModel(HardwareItem hw) {
+        TreeItem<SensorOrHardwareItemWrapper> treeItem = new TreeItem<>(new HardwareItemWrapper(hw));
+
+        for (Sensor sensor : hw.getSensors()) {
+            treeItem.getChildren().add(new TreeItem<>(new SensorWrapper(sensor)));
+        }
+        for (HardwareItem subHw : hw.getSubHardware()) {
+            treeItem.getChildren().add(createTreeTableModel(subHw));
+        }
+
+        return treeItem;
     }
 }
