f8a257935e3fea4d777a257d3dda1f00d823e4a6
Tuupertunut <superemppu@live.fi>
2018-12-03 06:35:25 +0200

    Implemented deleting fan curves.

--- src/main/java/com/github/tuupertunut/fanning/gui/FanCurvePane.java
+++ src/main/java/com/github/tuupertunut/fanning/gui/FanCurvePane.java
@@ -26,9 +26,14 @@ package com.github.tuupertunut.fanning.gui;
 import com.github.tuupertunut.fanning.core.FanCurve;
 import com.github.tuupertunut.fanning.core.FanningService;
 import java.io.IOException;
+import java.util.Optional;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import org.fxmisc.easybind.EasyBind;
@@ -44,6 +49,10 @@ public class FanCurvePane extends AnchorPane {
 
     @FXML
     private Label infoLabel;
+    @FXML
+    private Button deleteButton;
+    @FXML
+    private Button saveButton;
 
     public FanCurvePane(FanningService fanningService, ObservableValue<FanCurve> selectedFanCurveProperty) {
         this.fanningService = fanningService;
@@ -70,5 +79,12 @@ public class FanCurvePane extends AnchorPane {
                 return "Control " + selFanCurve.getTarget().getName() + " controlled by sensor " + selFanCurve.getSource().getName();
             }
         }));
+
+        deleteButton.setOnAction((ActionEvent event) -> {
+            Optional<ButtonType> result = new Alert(Alert.AlertType.CONFIRMATION, "Do you really want to delete this fan curve?", ButtonType.YES, ButtonType.CANCEL).showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.YES) {
+                fanningService.fanCurvesProperty().remove(selectedFanCurveProperty.getValue());
+            }
+        });
     }
 }
